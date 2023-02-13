ffc1b8fcf55b1fec9ee4fc9aae997e5514ab0bf2
rundqasem <61517810+rundqasem@users.noreply.github.com>
2023-02-12 19:41:58 -0500

    Add files via upload
    Change the import statement 
    to the logo to whatever matches your device's

--- screens/Signup.jsx
+++ screens/Signup.jsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button, Alert,Pressable,Image } from "react-native";
 import { useState } from "react";
 import { signup } from "../auth/auth_signup_password";
+import logo from "/Users/rundqasem/Desktop/LendAHand/assets/logo.png"
 
 function Signup() {
   const [email, setEmail] = useState("");
@@ -13,7 +14,7 @@ function Signup() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Signup</Text>
+      <Image source={logo} style={[styles.logo]} resizeMethod="contain"/>
       <TextInput
         style={styles.input}
         placeholder="Email"
@@ -27,14 +28,15 @@ function Signup() {
         value={password}
         secureTextEntry={true}
       />
-      <Button
-        title="Signup"
-        onPress={() => {
+
+<Pressable onPress={() => {
           console.log(email, password);
           signup(email, password);
           clearFields();
         }}
-      />
+          style={styles.button_sec}>
+            <Text style={styles.text_sec}>Sign Up</Text>
+        </Pressable>
     </View>
   );
 }
@@ -42,16 +44,36 @@ function Signup() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#cbc6c3",
     alignItems: "center",
     justifyContent: "center",
   },
   input: {
-    height: 40,
-    margin: 12,
+    width: '85%',
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 5,
+    borderColor: '#070B47',
     borderWidth: 1,
-    padding: 10,
+    borderRadius: 5,
   },
+  button_sec:{
+    width: '85%',
+    padding: 5,
+    marginVertical: 5,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  text_sec:{
+    fontWeight:'bold',
+    color: '#536878',
+    fontSize: 17,
+  },
+  logo:{
+    width: '70%',
+    maxWidth: 300,
+    maxHeight: 200,
+},
 });
 
 export { Signup };
