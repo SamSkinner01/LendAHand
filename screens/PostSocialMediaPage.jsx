f2d4bdc2f5cd8abb8ae4327a2156dcd9e7d7c6aa
unknown <saskinner19@gmail.com>
2023-02-20 20:11:16 -0500

    post is assocaited with a user

--- screens/PostSocialMediaPage.jsx
+++ screens/PostSocialMediaPage.jsx
@@ -1,55 +1,86 @@
-import { Button, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import { NavigationBar } from "../components/navigationBar";
-import {db} from "../auth/firebaseConfig";
+import { db, auth } from "../auth/firebaseConfig";
 import { useEffect, useState } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  orderBy,
+} from "firebase/firestore";
 import { useNavigation } from "@react-navigation/native";
 
 function PostSocialMediaPage() {
   const navigation = useNavigation();
+
   const [description, setDescription] = useState("");
   const [image, setImage] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
 
-  // posts to the database
+  async function getAuthUser() {
+    /*
+    Gets the current user's email via firebase authentication. 
+    Then, recieves the username from the database using the email. 
+    */
+    if (auth.currentUser.email) {
+      setEmail(auth.currentUser.email);
+    } else {
+      console.log("No user is signed in");
+      return;
+    }
+
+    const usersRef = collection(db, "users");
+    const q = query(usersRef, orderBy("email", "==", email));
+    const querySnapshot = await getDocs(q);
+    const temp_username = querySnapshot.docs[0].data().username;
+    setUsername(temp_username);
+  }
+
   async function postToDatabase() {
-    try{
-      const docRef = await addDoc(collection(db, "social_media_posts"), {
+    /*
+    Makes a social media post in the database.
+    */
+    try {
+      await addDoc(collection(db, "social_media_posts"), {
         description: description,
         image: image,
-        user: "user",
+        user: username,
         likes: 0,
         comments: [],
         time: new Date(),
       });
-      console.log("Document written with ID: ", docRef.id);
     } catch (error) {
       console.log(error);
     }
   }
 
+  useEffect(() => {
+    // Gets the current authenticated user and email on page load.
+    getAuthUser();
+  }, []);
+
   return (
     <>
-      
       <View style={styles.container}>
         <Text>Make A Post</Text>
-        <TextInput 
+        <TextInput
           onChangeText={(text) => setDescription(text)}
-          style={styles.container} 
-          placeholder="description" 
+          style={styles.container}
+          placeholder="description"
         />
-        <Pressable onPress={() => {
-          // post to database
-          postToDatabase();
-          // navigate back to social media page
-          navigation.navigate("Social Media");
-        }}
-          style={styles.container}>
-            <Text>Post</Text>
-          </Pressable>
+        <Pressable
+          onPress={() => {
+            postToDatabase();
+            navigation.navigate("Social Media");
+          }}
+          style={styles.container}
+        >
+          <Text>Post</Text>
+        </Pressable>
+      </View>
 
-    </View>
-    
-      {/* Navigation Bar */}
       <View>
         <NavigationBar />
       </View>
