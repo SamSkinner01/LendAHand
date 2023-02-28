ba9737c96f79ae0506bf278283877a50a53d213a
sam <saskinner19@gmail.com>
2023-02-28 12:28:39 -0500

    working auth and db for user

--- auth/auth_signup_password.js
+++ auth/auth_signup_password.js
@@ -3,19 +3,40 @@
 */
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebaseConfig.js";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { db } from "./firebaseConfig.js";
 
-const signup = async (email, password) => {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      console.log("You have signed up successfully!");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorCode, errorMessage);
-    });
-};
-
-export { signup };
+export const signup = (userDetails) => {
+    //deconstruct the users details we will need these later
+    const {username, firstname, lastname, email, password} = userDetails
+        //user firebase using the appropriate firebase method
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+            try{
+                addDoc(collection(db, "users"), {
+                    email: email,
+                    first_name: firstname,
+                    last_name: lastname,
+                    username: username,
+                    total_hours: 0,
+                    is_organization: false,
+                    social_media_posts : [],
+                    forum_posts : [],
+                    events_volunteered : [],
+                    signed_up_for_events: [],
+                    created_events: [],
+                    chat_rooms: [],
+                    friends: [],
+                    friend_requests: [],
+                    friend_requests_sent: [],
+                  });
+                  } catch (e) {
+                    console.error("Error adding document: ", e);
+                  }
+        })
+        //we need to catch the whole sign up process if it fails too.
+        .catch((error) => {
+            console.log("Error signing up: ", error)
+        }
+    )
+}
