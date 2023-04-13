fedba0ee241352da242e59b77702e5081b9cda59
sam <saskinner19@gmail.com>
2023-04-13 09:55:15 -0400

    Input Field added

--- components/ForumThreads.jsx
+++ components/ForumThreads.jsx
@@ -7,10 +7,12 @@
 */
 
 import { useRoute } from "@react-navigation/native";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import { useEffect, useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../auth/firebaseConfig";
+import { KeyboardAvoidingView } from "react-native";
+import { TextInput } from "react-native";
 
 export default function ForumThreads() {
   const route = useRoute();
@@ -18,6 +20,7 @@ export default function ForumThreads() {
 
   // will hold title, description, comments, etc
   const [data, setData] = useState([]);
+  const [textInput, setTextInput] = useState("");
 
   async function getForumData() {
     // get data from database given the forum_id,
@@ -36,24 +39,43 @@ export default function ForumThreads() {
     }
   }
 
+  async function handleSubmit() {
+    // Will add a comment to the database
+  }
+
   useEffect(() => {
     getForumData();
-    console.log(data);
   }, []);
 
   return (
     <View style={styles.container}>
       <View style={styles.top}>
-        <View style={styles.title}>
-          <Text>{data.title}</Text>
-        </View>
-        <View style={styles.description}>
-          <Text>{data.description}</Text>
-        </View>
-        <View style={styles.started_by}>
-          <Text>Thread started by: {data.username}</Text>
-        </View>
+        <Text style={styles.title}>{data.title}</Text>
+        <Text style={styles.description}>{data.description}</Text>
+        <Text style={styles.started_by}>
+          Thread started by: {data.username}
+        </Text>
       </View>
+
+      <View style={styles.middle}>
+        {
+          // contains comments
+        }
+      </View>
+
+      <KeyboardAvoidingView style={styles.bottom} behavior="padding">
+        <View style={styles.bottom_bar}>
+          <TextInput
+            //multiline={true}
+            style={styles.keyboard}
+            placeholder="Add a comment"
+            onChangeText={(text) => setTextInput(text)}
+          />
+          <Pressable style={styles.submit_button} onPress={handleSubmit}>
+            <Text>Submit</Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -64,15 +86,53 @@ const styles = StyleSheet.create({
   },
   top: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#00548e",
     justifyContent: "left",
     paddingTop: 70,
-    paddingLeft: 50,
+    paddingLeft: 30,
   },
   title: {
     fontSize: 30,
     fontWeight: "bold",
   },
-  description: {},
-  started_by: {},
+  description: {
+    fontSize: 20,
+    paddingTop: 10,
+  },
+  started_by: {
+    fontSize: 15,
+    paddingTop: 30,
+  },
+  middle: {
+    flex: 4,
+    backgroundColor: "#fff",
+  },
+  bottom: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  keyboard: {
+    backgroundColor: "#fff",
+    height: 50,
+    width: 350,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#00548e",
+    paddingLeft: 30,
+    paddingTop: 15,
+  },
+  submit_button: {
+    flexDirection: "column",
+    backgroundColor: "#00548e",
+    height: 50,
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#00548e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
