06d92593096a3239e5676276f4b8202e5d18bc2a
rundqasem <rqasem01@manhattan.edu>
2023-04-26 11:20:52 -0400

    Main Events page is finished with new assests

--- screens/DisplayAllEvents.jsx
+++ screens/DisplayAllEvents.jsx
@@ -1,9 +1,15 @@
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import { StyleSheet, Text, View, Pressable,Image,TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { NavigationBar } from "../components/navigationBar";
 import { useState, useEffect } from "react";
 import { readFromDb} from "../auth/firebaseConfig";
 import { RenderEvents } from "../components/RenderEvents";
+import food from "../assets/Food.webp"
+import edit from "../assets/Edit.png"
+import school from "../assets/school.png"
+import clean from "../assets/cleaning.png"
+import home from "../assets/home.png"
+
 
 function DisplayAllEvents () {
   const [events, setEvents] = useState([]);
@@ -11,7 +17,7 @@ function DisplayAllEvents () {
   useEffect(() => {
     async function getAllEvents() {
       const allEvents = await readFromDb('Events')
-      //setEvents(allEvents);
+      setEvents(allEvents);
     }
     
     const unsubscribe = navigation.addListener('focus', () => { // this was added because on naviagtion to this screen would not reload to show the updated database
@@ -23,35 +29,70 @@ function DisplayAllEvents () {
 
 
   return (
-    <View style={{flex:1,margin:80, flexDirection:'column'}}>
-      <View style={{marginTop:20}}> 
-        <Pressable  onPress={() => navigation.navigate("Post Event")}>
-          <Text>Create Event</Text>
-        </Pressable>
-        <Pressable  onPress={() => navigation.navigate('Search Results', { term: 'Food' })}>
-          <Text>Food</Text>
-        </Pressable>
-        <Pressable  onPress={() => navigation.navigate('Search Results', { term: 'Education' })}>
-          <Text>Education</Text>
-        </Pressable>
-        <Pressable  onPress={() => navigation.navigate('Search Results', { term: 'Sanitation' })}>
-          <Text>Sanitation</Text>
-        </Pressable>
-        <Pressable  onPress={() => navigation.navigate('Search Results', { term: 'Shelter' })}>
-          <Text>Shelter</Text>
-        </Pressable>
-      </View>
-      <View  style={{height:'80%'}}>
+    <>
+    <View style={styles.rowContainer}>
+        <TouchableOpacity  onPress={() => navigation.navigate("Post Event")}>
+        <Image source ={edit} style={styles.icons}  />
+        </TouchableOpacity> 
+        
+        <TouchableOpacity  onPress={() => navigation.navigate('Search Results', { term: 'Food' })}>
+        <Image source ={food} style={styles.icons}  />
+        </TouchableOpacity> 
+
+        <TouchableOpacity   onPress={() => navigation.navigate('Search Results', { term: 'Education' })}>
+        <Image source ={school} style={styles.icons}  />
+        </TouchableOpacity> 
+
+        <TouchableOpacity onPress={() => navigation.navigate('Search Results', { term: 'Sanitation' })}>
+        <Image source ={clean} style={styles.icons}  />
+        </TouchableOpacity> 
+
+        <TouchableOpacity  onPress={() => navigation.navigate('Search Results', { term: 'Shelter' })}>
+        <Image source ={home} style={styles.icons}  />
+        </TouchableOpacity> 
+
+         </View>
+         <View style={styles.line}></View>
+   
+    
+    <View style={styles.container}>
+     
         <RenderEvents allEvents={events} />
-      </View>
+     
       <View>
         <NavigationBar />
       </View>
     </View>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#cbc6c3",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  rowContainer: {
+    flexDirection: "row",
+    height: '9%',
+    justifyContent: 'space-evenly',
+    paddingTop: '11%',
+    backgroundColor:'#00548e',
+    marginVertical: 0
+  },
+  line: {
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    marginVertical: 0,
+  },
+  icons:{
+    maxWidth: 25,
+    maxHeight: 25,
+},
+
+
 
 })
 
