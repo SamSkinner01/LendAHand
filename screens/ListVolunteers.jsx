e86bf9eda26cc7fc56f639bbd9e3a6f36c9fb627
Kogara13 <kieranogara@gmail.com>
2023-05-02 17:47:44 -0400

    attempted different imports and route params

--- screens/ListVolunteers.jsx
+++ screens/ListVolunteers.jsx
@@ -8,24 +8,27 @@ import {
   } from "react-native";
 import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { auth, db, getVolunteers, isOrganization, readFromDb, updateEvent} from "../auth/firebaseConfig";
+import { auth, db, findVolunteers, isOrganization, readFromDb, updateEvent} from "../auth/firebaseConfig";
 import { deleteCollection, add_to_array } from "../auth/firebaseConfig";
 import { collection, getDocs, where, query } from "firebase/firestore";
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import back from "../assets/back.png";
 import { ScrollView } from "react-native-web";
-import RenderVolunteers from "../components/RenderVolunteers";
+import { RenderVolunteers } from "../components/RenderVolunteers";
+import { useRoute } from "@react-navigation/native";
 
-const DisplaySingularEvent = ({ item }) => {
-    volunteers = findVolunteers(item.id);
-
-    <View style={styles.container}> 
+const DisplaySingularEvent = ({ item, route }) => {
+    // route.params = {item: item};
+    const volunteers = findVolunteers(item.id);
+    <View> 
         <Text>Volunteers</Text>
         <View>
             <RenderVolunteers array={volunteers}/>
         </View> 
     </View>
 }
+
+
   
 export default { DisplaySingularEvent };
